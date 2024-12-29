d9d655047c10b9d6b0fb9cd070318f3d35a1e173
Fares <faresmuhammed75@gmail.com>
2024-12-29 16:55:39 +0200

    update

--- src/Components/AboutUsPage/AboutUs.jsx
+++ src/Components/AboutUsPage/AboutUs.jsx
@@ -6,18 +6,18 @@ export default function AboutUs() {
         {/* <div className="py-5 p-4 mt-5 d-flex flex-wrap gap-md-4 border-bottom">
 </div> */}
 
-      <div className="py-5 p-4 mt-5 d-flex flex-wrap gap-md-4 border-bottom">
-        <div className="d-flex flex-wrap" >
-          <div className=" col-lg-6 col-12 ">
+      <div className="py-lg-2 p-4 mt-5 d-flex flex-wrap gap-md-4 border-bottom">
+        <div className="d-flex flex-wrap mt-5" >
+          <div className=" col-lg-7 col-12 ">
             <div className="p-lg-5 p-1 py-md-5 mb-5">
 
-              <h2 className="mb-4">
+              <h2>
                 SERVICES
               </h2>
               <p className="mb-4">
-                <p>Training and Expert Oil and Gas Solutions by Streamlines Energy Enterprise              </p>
-                <p>At Streamlines Energy we focus and offer extensive technical training courses by expertise instructors of oil & gas sectors.  In addition to Specialized field development studies, field optimization studies, testing and operations services.              </p>
-                <p>Our expert team combines industry knowledge with production enhancement acumen to deliver tailored solutions that drive success for our clients.              </p>
+                Training and Expert Oil and Gas Solutions by Streamlines Energy Enterprise.<br/>
+                At Streamlines Energy we focus and offer extensive technical training courses by expertise instructors of oil & gas sectors.  In addition to Specialized field development studies, field optimization studies, testing and operations services.<br/>
+                Our expert team combines industry knowledge with production enhancement acumen to deliver tailored solutions that drive success for our clients.<br/>
               </p>
               <p className="mb-1 fw-bold">
                 •	Training <br/>
@@ -27,25 +27,25 @@ export default function AboutUs() {
               </p>
           </div>
         </div>
-        <div className="col-lg-6 col-12 d-flex align-items-center justify-content-center">
-          <img className="img1" src={"compound-balance-beam-pump-manufacturers-1.jpg"}/>
+        <div className="col-lg-5 col-12 d-flex align-items-center justify-content-center">
+          <img className="img1" src={"Oil-and-Gas-istock-jpg-ezgif.com-webp-to-jpg-converter.jpg"}/>
         </div>
       </div>
       </div>
 
-      <div className=" p-4 d-flex flex-wrap gap-md-4 border-bottom">
+      <div className=" py-lg-2 p p-4 d-flex flex-wrap gap-md-4 border-bottom">
         <div className="d-flex flex-wrap" >
-          <div className=" col-lg-6 col-12 ">
+          <div className=" col-lg-7 col-12 ">
             <div className="p-lg-5 p-1 py-md-5 mb-5">
 
-              <h2 className="mb-4">
+              <h2 className="">
               OUR TEAM
               </h2>
               <p className="mb-4">
-                <p>With more than 40 years of combined experience in oil and gas chain from leading companies, we focus on providing clients with solutions that maximize value in a rapidly evolving oil & gas sector.</p>
-                <p>Our extensive network allows us to identify and execute the best opportunities for training, field development, field study, field optimization, testing, and operations.</p>
-                <p>Our professional team consists of experienced managers, technical specialists, and instructors from reputable local engineering partners.</p>
-                <p>Together, we assist our clients in successfully managing multidisciplinary studies, field development, and field optimization. We focus on providing clients with solutions that maximize value in the rapidly evolving oil and gas sectors.</p>
+                With more than 40 years of combined experience in oil and gas chain from leading companies, we focus on providing clients with solutions that maximize value in a rapidly evolving oil & gas sector.<br/>
+                Our extensive network allows us to identify and execute the best opportunities for training, field development, field study, field optimization, testing, and operations.<br/>
+                Our professional team consists of experienced managers, technical specialists, and instructors from reputable local engineering partners.<br/>
+                Together, we assist our clients in successfully managing multidisciplinary studies, field development, and field optimization. We focus on providing clients with solutions that maximize value in the rapidly evolving oil and gas sectors.<br/>
               </p>
               <p className="mb-1 fw-bold">
                 •	Growth Manager<br/>
@@ -57,8 +57,8 @@ export default function AboutUs() {
               </p>
           </div>
         </div>
-        <div className="col-lg-6 col-12 d-flex align-items-center justify-content-center">
-          <img className="img1" src={"Screenshot 2024-12-23 133136.png"}/>
+        <div className="col-lg-5 col-12 d-flex align-items-center justify-content-center">
+          <img className="img1" src={"Our Team.jpg "} />
         </div>
       </div>
       </div>
@@ -66,43 +66,37 @@ export default function AboutUs() {
 
 
 
-      <div className="p-lg-5 "
+      <div className="p-lg-5 py-lg-3"
         style={{  display: 'flex' , flexDirection: 'column'}}
       >
 
-        <div className="col-12 p-4">
-            <div
+      <div className="col-12 p-4">
+          <div className="p-3" style={{backgroundColor: '#EAEAEA' , borderRadius: '15px'}}>
+          
+          <div
               className="d-flex align-items-center"
             >
-              <h4 style={{ backgroundColor: "#BF2A2A" , color: 'white' , width: '150px'}} className="center p-1">
-              HOME
-              </h4>
-            </div>
-            <p className="mb-4">
-                <p>Powering the world with Streamlines Energy Enterprise. We provide innovative and sustainable solutions for oil & gas industry.                </p>
-              </p>
-          </div>
-
-        <div className="col-12 p-4"  style={{marginTop: '-30px'}}>
-            <div
-              className="d-flex align-items-center"
-            >
-              <h4 style={{ backgroundColor: "#BF2A2A" , color: 'white' , width: '150px'}} className="center p-1">
+              <h4 style={{ color: '#1877F2' , width: '150px' , fontWeight: 'bold'}} className=" p-1">
               ABOUT US
               </h4>
             </div>
             <p className="mb-4">
-                <p>We are your Trusted Partner in the Oil and Gas Industry.                </p>
-                <p>Streamlines Energy is a service and supportive company focused on oil and gas sectors. We specialize in Training, Software Modelling, Field Development and Field Optimization Study, Testing and operations.                </p>
-                <p>Our unique approach combines corporate Training, operational, and technical expertise across various oil/gas sectors.                </p>
+                We are your Trusted Partner in the Oil and Gas Industry.<br/>              
+                Our unique approach combines corporate Training, operational, and technical expertise across various oil/gas sectors.<br/>
+                Gas Industry
+                Streamlines Energy is a service and supportive company focused on oil and gas sectors. We specialize in Training, Software Modelling, Field Development and Field Optimization Study, Testing and operations.
+                i am so cooked, baked, toasted, deep fried, air fried, grilled and burnt.
               </p>
           </div>
+          </div>
 
-          <div className="col-12 p-4" style={{marginTop: '-30px'}}>
-            <div
+
+        <div className="col-12 p-4">
+          <div className="p-3" style={{backgroundColor: '#EAEAEA' , borderRadius: '15px'}}>
+          <div
               className="d-flex align-items-center"
             >
-              <h4 style={{ backgroundColor: "#BF2A2A" , color: 'white' , width: '150px'}} className="center p-1">
+              <h4 style={{ color: '#1877F2' , width: '150px' , fontWeight: 'bold' }} className=" p-1 ">
               VISION
               </h4>
             </div>
@@ -111,11 +105,18 @@ export default function AboutUs() {
               Development the way to get started is to quit talking and begin doing.
             </p>
           </div>
-          <div className="col-12 p-4" style={{marginTop: '-30px'}}>
-            <div
+        </div>
+
+
+          
+
+
+        <div className="col-12 p-4">
+          <div className="p-3" style={{backgroundColor: '#EAEAEA' , borderRadius: '15px'}}>
+          <div
               className="d-flex align-items-center"
             >
-              <h4 style={{ backgroundColor: "#BF2A2A" , color: 'white' , width: '150px'}} className="center p-1">
+              <h4 style={{ color: '#1877F2' , width: '150px' , fontWeight: 'bold'}} className=" p-1">
               MISSION
               </h4>
             </div>
@@ -123,6 +124,8 @@ export default function AboutUs() {
             Evolve our business to add value to our people, customers, and shareholders by following the highest safety, technological, and operational standards.
             </p>
           </div>
+          </div>
+
         </div>
 
       <Footer className={"color-white"}/>
