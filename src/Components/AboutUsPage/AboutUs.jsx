8b627b11596dee927088a537e179cf5e899daf55
Fares <faresmuhammed75@gmail.com>
2024-12-30 10:01:45 +0200

    update

--- src/Components/AboutUsPage/AboutUs.jsx
+++ src/Components/AboutUsPage/AboutUs.jsx
@@ -11,20 +11,35 @@ export default function AboutUs() {
           <div className=" col-lg-7 col-12 ">
             <div className="p-lg-5 p-1 py-md-5 mb-5">
 
-              <h2>
+              <h2 className="mb-4">
                 SERVICES
               </h2>
               <p className="mb-4">
-                Training and Expert Oil and Gas Solutions by Streamlines Energy Enterprise.<br/>
-                At Streamlines Energy we focus and offer extensive technical training courses by expertise instructors of oil & gas sectors.  In addition to Specialized field development studies, field optimization studies, testing and operations services.<br/>
-                Our expert team combines industry knowledge with production enhancement acumen to deliver tailored solutions that drive success for our clients.<br/>
+                <p className="mb-2">Training and Expert Oil and Gas Solutions by Streamlines Energy Enterprise.<br/></p>
+                <p className="mb-2">At Streamlines Energy we focus and offer extensive technical training courses by expertise instructors of oil & gas sectors.  In addition to Specialized field development studies, field optimization studies, testing and operations services.<br/></p>
+                <p className="mb-5">Our expert team combines industry knowledge with production enhancement acumen to deliver tailored solutions that drive success for our clients.<br/></p>
               </p>
-              <p className="mb-1 fw-bold">
-                •	Training <br/>
-                •	Software <br/>
-                •	Consultation<br/>
-                •	Operations<br/>
-              </p>
+
+              <div className="col-12 flex-wrap gap-3" style={{display: 'flex' , justifyContent: 'space-between'}}>
+                <div className="col-md-5 col-12"  style={{backgroundColor: '#EAEAEA' , borderRadius: '5px' , height: '36px'}}>
+                  <p className="fw-bold p-1 ">  •  Training </p>
+                </div>
+                <div className="col-md-5 col-12" style={{backgroundColor: '#EAEAEA' , borderRadius: '5px', height: '36px'}}>
+                  <p className="fw-bold p-1">  •	Software </p>
+                </div>
+                <div className="col-md-5 col-12" style={{backgroundColor: '#EAEAEA' , borderRadius: '5px', height: '36px'}}>
+                  <p className="fw-bold p-1">  •	Consultation </p>
+                </div>
+                <div className="col-md-5 col-12" style={{backgroundColor: '#EAEAEA' , borderRadius: '5px', height: '36px'}}>
+                  <p className="fw-bold p-1">  •	Operations </p>
+                </div>
+              </div>
+              {/* <p className="mb-1 fw-bold">
+              <br/>
+                 <br/>
+                <br/>
+                <br/>
+              </p> */}
           </div>
         </div>
         <div className="col-lg-5 col-12 d-flex align-items-center justify-content-center">
@@ -38,23 +53,42 @@ export default function AboutUs() {
           <div className=" col-lg-7 col-12 ">
             <div className="p-lg-5 p-1 py-md-5 mb-5">
 
-              <h2 className="">
+              <h2 className="mb-4">
               OUR TEAM
               </h2>
-              <p className="mb-4">
+              <p className="mb-5">
                 With more than 40 years of combined experience in oil and gas chain from leading companies, we focus on providing clients with solutions that maximize value in a rapidly evolving oil & gas sector.<br/>
                 Our extensive network allows us to identify and execute the best opportunities for training, field development, field study, field optimization, testing, and operations.<br/>
                 Our professional team consists of experienced managers, technical specialists, and instructors from reputable local engineering partners.<br/>
                 Together, we assist our clients in successfully managing multidisciplinary studies, field development, and field optimization. We focus on providing clients with solutions that maximize value in the rapidly evolving oil and gas sectors.<br/>
               </p>
-              <p className="mb-1 fw-bold">
+
+              
+              <div className="col-12 flex-wrap gap-3" style={{display: 'flex' , justifyContent: 'space-between'}}>
+                <div className="col-md-5 col-12"  style={{backgroundColor: '#EAEAEA' , borderRadius: '5px' , height: '36px'}}>
+                  <p className="fw-bold p-1 ">  •  Growth Manager </p>
+                </div>
+                <div className="col-md-5 col-12" style={{backgroundColor: '#EAEAEA' , borderRadius: '5px', height: '36px'}}>
+                  <p className="fw-bold p-1"> •	General Manager </p>
+                </div>
+                <div className="col-md-5 col-12" style={{backgroundColor: '#EAEAEA' , borderRadius: '5px', height: '36px'}}>
+                  <p className="fw-bold p-1">  •	Service Manage </p>
+                </div>
+                <div className="col-md-5 col-12" style={{backgroundColor: '#EAEAEA' , borderRadius: '5px', height: '36px'}}>
+                  <p className="fw-bold p-1">  •	Support & HR Team </p>
+                </div>
+                <div className="col-md-5 col-12" style={{backgroundColor: '#EAEAEA' , borderRadius: '5px', height: '36px'}}>
+                  <p className="fw-bold p-1">   •	Accounting Team </p>
+                </div>
+              </div>
+              {/* <p className="mb-1 fw-bold">
                 •	Growth Manager<br/>
                 •	General Manager<br/>
                 •	Service Manager<br/>
                 •	Technical Team<br/>
                 •	Support & HR Team<br/>
                 •	Accounting Team<br/>
-              </p>
+              </p> */}
           </div>
         </div>
         <div className="col-lg-5 col-12 d-flex align-items-center justify-content-center">
@@ -76,7 +110,7 @@ export default function AboutUs() {
           <div
               className="d-flex align-items-center"
             >
-              <h4 style={{ color: '#1877F2' , width: '150px' , fontWeight: 'bold'}} className=" p-1">
+              <h4 style={{ color: '#092140' , width: '150px' , fontWeight: 'bold'}} className=" p-1">
               ABOUT US
               </h4>
             </div>
@@ -96,7 +130,7 @@ export default function AboutUs() {
           <div
               className="d-flex align-items-center"
             >
-              <h4 style={{ color: '#1877F2' , width: '150px' , fontWeight: 'bold' }} className=" p-1 ">
+              <h4 style={{ color: '#092140' , width: '150px' , fontWeight: 'bold' }} className=" p-1 ">
               VISION
               </h4>
             </div>
@@ -116,7 +150,7 @@ export default function AboutUs() {
           <div
               className="d-flex align-items-center"
             >
-              <h4 style={{ color: '#1877F2' , width: '150px' , fontWeight: 'bold'}} className=" p-1">
+              <h4 style={{ color: '#092140' , width: '150px' , fontWeight: 'bold'}} className=" p-1">
               MISSION
               </h4>
             </div>
