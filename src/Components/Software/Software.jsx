7c325f6419300cf88c3aa0d7a641a412dcaf20c3
Fares <faresmuhammed75@gmail.com>
2025-01-13 12:14:07 +0200

    update

--- src/Components/Software/Software.jsx
+++ src/Components/Software/Software.jsx
@@ -7,10 +7,14 @@ export default function Software() {
     return(
         <>
         <div className="mt-5 ">
-            <div className=" flex-wrap p-3 p-md-5 col-12 border-bottom" style={{ color: "#EDEDED" }}>
-                    <Link to={"https://rfdyn.com/"} target="_blank">
-                        <h1 className="mb-4 d-flex align-items-center mt-5" style={{color: '#0F2647'  , height: '60px'}}>tNavigator</h1>
+            <div className="center flex-wrap p-3 p-md-5 col-12 border-bottom" style={{ color: "#EDEDED" }}>
+            <div className="ps">
+                    <Link  to={"https://rfdyn.com/"} target="_blank">
+                        <h1  className="sh  d-flex align-items-center mt-5" style={{ height: '60px'}}>tNavigator</h1>
                     </Link>
+                    <p style={{color :'#060606' }}>A powerful simulation software that supports reservoir modeling, production forecasting,
+                        and strategic field planning, tNavigator enables data-driven decisions with high accuracy.
+                    </p>
                     <img  
                         className="w-100 mb-4"
                         src={"WhatsApp Image 2025-01-13 at 10.57.17_1e83157f.jpg"}
@@ -19,16 +23,19 @@ export default function Software() {
                             // height: '500px'
                         }}
                     />
-                        <p style={{color :'#060606'}}>A powerful simulation software that supports reservoir modeling, production forecasting,
-                        and strategic field planning, tNavigator enables data-driven decisions with high accuracy.
-                        </p>
+                </div>
             </div>
 
             <div className=" flex-wrap p-3 p-md-5 col-12 border-bottom" style={{ color: "#EDEDED" }}>
-                    <Link to={"https://www.peloton.com/"} target="_blank">
-                        <h1 className="mb-4 d-flex align-items-center " style={{color: '#0F2647'  , height: '60px'}}>Peloton</h1>
+            <div className="ps">
+
+                    <Link  to={"https://www.peloton.com/"} target="_blank">
+                        <h1  className="sh  d-flex align-items-center " style={{ height: '60px'}}>Peloton</h1>
                     </Link>
-                    <img  
+                    <p style={{color :'#060606' }}>The Peloton platform integrates real-time well and production management, enhancing
+                        operational efficiency from drilling through production phases.
+                        </p>
+                    <img
                         className="w-100 mb-4"
                         src={"Introducing_WV_Allez (2).jpg"}
                             style={{
@@ -36,15 +43,21 @@ export default function Software() {
                             // height: '300px'
                         }}
                     />
-                        <p style={{color :'#060606' }}>The Peloton platform integrates real-time well and production management, enhancing
-                        operational efficiency from drilling through production phases.
-                        </p>
+
+            </div>
             </div>
 
+
             <div className=" flex-wrap  p-3 p-md-5 col-12  border-bottom" style={{ color: "#EDEDED" }}>
-                    <Link to={"https://www.tachyus.com/"} target="_blank">
-                        <h1 className="mb-4 d-flex align-items-center" style={{color: '#0F2647'  , height: '60px'}}>Tachyus</h1>
+            <div className="ps">
+
+                    <Link  to={"https://www.tachyus.com/"} target="_blank">
+                        <h1  className="sh d-flex align-items-center" style={{ height: '60px'}}>Tachyus</h1>
                     </Link>
+                    <p style={{color :'#060606' }}>Aurion offers a GHG emissions management solution that streamlines emissions reporting,
+                                reduces carbon footprint, enhances efficiency, and ensures transparent and auditable
+                                practices.
+                        </p>
                     <img  
                         className="w-100 mb-4"
                         src={"1506097082-fd6b03607788541e45ff5289d9eed2865f4e4890fdeb930caaacd1ee054431be-d_1280.jpg"}
@@ -53,16 +66,19 @@ export default function Software() {
                             // height: '300px'
                         }}
                     />
-                        <p style={{color :'#060606' }}>Aurion offers a GHG emissions management solution that streamlines emissions reporting,
-                                reduces carbon footprint, enhances efficiency, and ensures transparent and auditable
-                                practices.
-                        </p>
+
+            </div>
             </div>
 
             <div className=" flex-wrap p-3 p-md-5 col-12 border-bottom" style={{ color: "#EDEDED" }}>
-                    <Link to={"https://www.wellirs.com/"} target="_blank">
-                        <h1 className="mb-4 d-flex align-items-center" style={{color: '#0F2647'  , height: '60px'}}>WelliRs</h1>
+            <div className="ps">
+
+                    <Link  to={"https://www.wellirs.com/"} target="_blank">
+                        <h1  className="sh d-flex align-items-center" style={{ height: '60px'}}>WelliRs</h1>
                     </Link>
+                    <p style={{color :'#060606' }}>WelliRs provides a comprehensive well integrity management system to ensure safe and
+                        compliant operations throughout the well lifecycle.
+                    </p>
                     <img  
                         className="w-100 mb-4"
                         src={"WhatsApp Image 2025-01-13 at 10.57.32_66d6d248.jpg"}
@@ -71,28 +87,33 @@ export default function Software() {
                             // height: '300px'
                         }}
                     />
-                        <p style={{color :'#060606' }}>WelliRs provides a comprehensive well integrity management system to ensure safe and
-                        compliant operations throughout the well lifecycle.
-                        </p>
+
+            </div>
             </div>
 
+
             <div className=" flex-wrap p-3 p-md-5 col-12 border-bottom" style={{ color: "#EDEDED" }}>
-                    <Link to={"https://three60energy.com/"} target="_blank">
-                        <h1 className="mb-4 d-flex align-items-center" style={{color: '#0F2647'  , height: '60px'}}>Poseidon</h1>
+                <div className="ps">
+                
+                    <Link  to={"https://three60energy.com/"} target="_blank">
+                        <h1  className="sh d-flex align-items-center" style={{ height: '60px'}}>Poseidon</h1>
                     </Link>
+                    <p style={{color :'#060606' }}>Poseidon software facilitates data processing and analysis, providing an intuitive platform
+                        for managing reservoirs and making informed decisions.
+                        </p>
                     <img  
                         className="w-100 mb-4"
                         src={"Technology_oil_gas_subsurface_reservoir_modelling_Poseidon2.png.jpg"}
                             style={{
                             objectFit: "cover",
-                            // height: '300px'
+                            // height: '500px'
                         }}
                     />
-                        <p style={{color :'#060606' }}>Poseidon software facilitates data processing and analysis, providing an intuitive platform
-                        for managing reservoirs and making informed decisions.
-                        </p>
+
             </div>
-            </div>
+            </div>        
+        </div>
+
 
         <Footer/>
 
